593b395f2187102424d86c49f36ecf312274795e
Andrew Barger <asbarg01@gmail.com>
2024-08-31 13:54:11 -0400

    Set up pipeline

---
+++ build/Build-DockerContainer.ps1
@@ -0,0 +1,39 @@
+[CmdletBinding()]
+param(
+    [string]
+    [Parameter()]
+    $DockerFilePath = $Env:DOCKERFILE_PATH,
+
+    [Parameter()]
+    [string]$DockerImageName = $ENV:DOCKER_IMAGE_NAME,
+
+    [Parameter()]
+    [string]$DockerImageTag = $ENV:DOCKER_IMAGE_TAG,
+
+    [Parameter()]
+    [string]$GcloudLocation = $ENV:GCLOUD_LOCATION,
+
+    [Parameter()]
+    [string]$GcloudProject = $ENV:GCLOUD_PROJECT,
+
+    [Parameter()]
+    [string]$GcloudArtifactRegistryRepo = $ENV:GCLOUD_ARTIFACT_REGISTRY_REPO, 
+
+    [string]
+    [Parameter()]
+    $GcloudAuthKeyFilePath = $ENV:GCLOUD_AUTH_KEY_FILE_PATH
+)
+
+
+$DockerRepoDomain = "$GCloudLocation-docker.pkg.dev"
+$ImageTag = "$DockerRepoDomain/$GCloudProject/$GcloudArtifactRegistryRepo/$($DockerImageName):$DockerImageTag"
+Write-Host "Full Docker Image Tag is $ImageTag"
+
+$DockerLoginUrl = "https://$DockerRepoDomain"
+
+Get-Content -Path $GcloudAuthKeyFilePath | 
+  docker login -u _json_key --password-stdin $DockerLoginUrl
+
+& docker buildx build -t $ImageTag $DockerFilePath
+
+& docker push $ImageTag
